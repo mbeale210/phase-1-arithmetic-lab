3f19ec0b30e0aaf4d9250826eff2bb41d13037e9
Mark Beale <bealebeaverst@gmail.com>
2024-07-15 15:39:49 -0700

    Complete Lab

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply)
 
+let random = Math.floor(Math.random() * 10)+1;
+console.log(random)
+
+let num3 = 16
+let num4 = 6
+let mod = num3 % num4
+console.log(mod)
+
+let max = Math.max(1,4,8,12,16,20)
+console.log(max)
 // Write your code here
